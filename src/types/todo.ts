34d42a8bd6a78b6d6491bd39987b696e741fa046
Lauris-21 <laurasanchez0301@gmail.com>
2023-01-21 14:09:54 +0100

    Fix

--- src/types/todo.ts
+++ src/types/todo.ts
@@ -1,24 +1,22 @@
 export interface Todo {
-  task: string
-  description: string
-  id: string
+  task: string;
+  description: string;
+  id: string;
 }
 
 export type HandleFunction = (
-  e:
-    | React.ChangeEvent<HTMLInputElement>
-    | React.ChangeEvent<HTMLTextAreaElement>
-) => void
+  e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>,
+) => void;
 
 export interface Props {
-  children: JSX.Element | JSX.Element[]
+  children: JSX.Element | JSX.Element[];
 }
 
 export interface Prop {
-  localSt: Todo[]
+  localSt: Todo[];
 }
 
 export interface TasksContext {
-  data: Todo[]
-  setData: any
+  data: Todo[];
+  setData: any;
 }
